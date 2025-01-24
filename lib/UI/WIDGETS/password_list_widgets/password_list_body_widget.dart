efa8bf52beed6324e1bbb49dbef2aa59cf54dbed
dorkospron0 <tareassec9@gmail.com>
2025-01-23 21:54:03 -0500

    Fix build.
    Scaffold with bg color and android build.

--- lib/UI/WIDGETS/password_list_widgets/password_list_body_widget.dart
+++ lib/UI/WIDGETS/password_list_widgets/password_list_body_widget.dart
@@ -8,61 +8,59 @@ class PasswordListBodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 30,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 30,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            "Tus favoritas",
+            style: Theme.of(context).textTheme.titleMedium,
           ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              "Tus favoritas",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
+        ),
+        ListPasswords(
+          size: size,
+          image: Image.asset('assets/images/adobe.png'),
+          titleCard: "Title",
+          descriptionCard: "Description",
+        ),
+        SizedBox(
+          height: 50,
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+          child: Text(
+            "Todas tus contraseñas",
+            style: Theme.of(context).textTheme.titleMedium,
           ),
-          ListPasswords(
-            size: size,
-            image: Image.asset('assets/images/adobe.png'),
-            titleCard: "Title",
-            descriptionCard: "Description",
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-            child: Text(
-              "Todas tus contraseñas",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              height: size.height * .3,
-              child: ListView.builder(
-                itemCount: 8,
-                scrollDirection: Axis.vertical,
-                itemBuilder: (context, index) => Column(
-                  children: [
-                    PasswordItem(
-                      title: "Behance",
-                      imageURL: "assets/images/be.png",
-                    ),
-                    Divider(
-                      color: Color.fromARGB(255, 82, 82, 82),
-                      height: 20.0,
-                    )
-                  ],
-                ),
-                shrinkWrap: true,
+        ),
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            height: size.height * .3,
+            child: ListView.builder(
+              itemCount: 8,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) => Column(
+                children: [
+                  PasswordItem(
+                    title: "Behance",
+                    imageURL: "assets/images/be.png",
+                  ),
+                  Divider(
+                    color: Color.fromARGB(255, 82, 82, 82),
+                    height: 20.0,
+                  )
+                ],
               ),
+              shrinkWrap: true,
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
